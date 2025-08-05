72501d3d6d998064768d0f8a2bd7a86a2026147d
ValDin08 <valentin.dinon@gmail.com>
2025-08-05 09:11:34 +0200

    Update serveurBucheron 3.0.lua

--- serveurBucheron.lua
+++ serveurBucheron.lua
@@ -1,6 +1,7 @@
 --Déclaration des variables
 	--Globales
-	local ServerVersion	=	"2.0"		--Version du serveur
+	local ServerVersion	=	"3.0"		--Version du serveur
+	local TurtleVersion	=	"0.0"			--Version de la turtle
 	
 	--Réseau
 	local LocalID			=	11			--ID du serveur
@@ -19,7 +20,7 @@
 	--Infos turtle
 	local TurtleLastPosition	=	{0, 0, 0}	--Dernière position connue de la turtle
 	local TurtleLastOrientation	=	0			--Dernière orientation connue de la turtle
-	local LastOrientationString 	= 	""			--Dernière orientation connue de la turtle convertie en string
+	local LastOrientationString = 	""			--Dernière orientation connue de la turtle convertie en string
 	local HarvestedTrees		=	0			--Nombre d'arbres abattus sur le tir en cours
 	local CurrentFuelLevel		=	0			--Niveau de carburant actuel
 
@@ -27,10 +28,16 @@
 	local FuelChestFillingLevel	=	0			--Niveau de remplissage du coffre tampon de carburant
 	local WoodChestFillingLevel	=	0			--Niveau de remplissage du coffre stock des buches
 
+	--Ecran
+	local ScreenSide			=	"left"		--Côté de l'écran
+	
+	--Commandes
+	local RedstoneInputSide		=	"bottom"	--Côté de l'entrée du signal redstone	
 
 --Programme
 print("Version serveur : "..ServerVersion..".")
 rednet.open(ModemSide)
+local HMI = peripheral.wrap(ScreenSide)
 
 while true do
 	ID, Message = rednet.receive(15)
@@ -57,10 +64,12 @@ while true do
 				end
 			--Autorisation révoquée
 			elseif not ServerAuthorizing and TurtleAuthorized then
+				rednet.send(TurtleID, {AnswerID = 202})
 				print("Autorisation de travail révoquée à turtle ID #"..TurtleID..".")
 				TurtleAuthorized = false
 			--Interdiction de travail
 			else
+				rednet.send(TurtleID, {AnswerID = 202})
 				print("Interdiction de travail pour turtle ID #"..TurtleID..".")
 			end
 		
@@ -82,6 +91,8 @@ while true do
 				HarvestedTrees = Message.TreeHarvestedCurrentRun
 				print("Carburant restant dans le réservoir : "..CurrentFuelLevel..".")
 			end
+			
+			TurtleVersion = Message.ProgramVersion
 		end
 
 	elseif ID == ChestRelayID then
@@ -120,10 +131,86 @@ while true do
 	end
 	
 	--Autorisation de travail accordée à la turtle
-	if WoodChestFillingLevel < 100 then
+	if WoodChestFillingLevel < 100 and not rs.getInput(RedstoneInputSide) then
 		ServerAuthorizing = true
 	else
 		ServerAuthorizing = false
 	end		
 
-end
+	--Gestion de l'écran
+	HMI.clear()
+	HMI.setCursorPos(1,1)
+	HMI.setTextScale(0.5)
+	
+	HMI.setBackgroundColor(colors.black)
+	
+	HMI.write("Serveur: "..ServerVersion.." / Turtle : "..TurtleVersion)
+	
+	--Affichage de l'état de connexion de la turtle
+	HMI.setCursorPos(1,2)
+	HMI.write("Turtle connectée : ")
+	if TurtleConnected then
+		HMI.setBackgroundColor(colors.green)
+		HMI.write("OUI")
+	else
+		HMI.setBackgroundColor(colors.red)
+		HMI.write("NON")
+	end
+	HMI.setBackgroundColor(colors.black)
+	
+	--Affichage de l'état de fonctionnement de la turtle
+	HMI.setCursorPos(1,3)
+	HMI.write("Turtle autorisée à travailler : ")
+	if TurtleAuthorized then
+		HMI.setBackgroundColor(colors.green)
+		HMI.write("OUI")
+	else
+		HMI.setBackgroundColor(colors.red)
+		HMI.write("NON")
+	end
+	HMI.setBackgroundColor(colors.black)
+	
+	--Affichage de la position de la turtle
+	HMI.setCursorPos(1,4)
+	HMI.write("Position turtle : "..TurtleLastPosition[1]..", "..TurtleLastPosition[2]..", "..TurtleLastPosition[3])
+	HMI.setCursorPos(1,5)
+	HMI.write("Allant vers : "..LastOrientationString)
+	
+	--Affichage du carburant restant
+	HMI.setCursorPos(1,6)
+	HMI.write("Carburant restant = "..CurrentFuelLevel)
+	
+	--Affichage du nombre d'arbres coupés
+	HMI.setCursorPos(1,7)
+	HMI.write("Nombre d'arbres abattus : "..HarvestedTrees)	
+	
+	--Affichage de l'état de connexion au relais
+	HMI.setCursorPos(1,8)
+	HMI.write("Relais connecté : ")
+	if ChestRelayConnected then
+		HMI.setBackgroundColor(colors.green)
+		HMI.write("OUI")
+	else
+		HMI.setBackgroundColor(colors.red)
+		HMI.write("NON")
+	end
+	HMI.setBackgroundColor(colors.black)
+	
+	--Affichage de l'état de remplissage des coffres
+	HMI.setCursorPos(1,9)
+	if WoodChestFillingLevel > 50 and WoodChestFillingLevel < 85 then HMI.setBackgroundColor(colors.yellow)
+	elseif WoodChestFillingLevel >= 85 then HMI.setBackgroundColor(colors.red)
+	elseif WoodChestFillingLevel <=50 then HMI.setBackgroundColor(colors.green)
+	end
+	HMI.write("Coffre buches = "..WoodChestFillingLevel.."%")
+	HMI.setBackgroundColor(colors.black)
+	
+	HMI.setCursorPos(1,10)
+	if FuelChestFillingLevel >= 10 and FuelChestFillingLevel < 45 then HMI.setBackgroundColor(colors.yellow)		
+	elseif FuelChestFillingLevel < 10 then HMI.setBackgroundColor(colors.red)		
+	elseif FuelChestFillingLevel >= 45 then HMI.setBackgroundColor(colors.green)		
+	end
+	HMI.write("Coffre carburant = "..FuelChestFillingLevel.."%")	
+	HMI.setBackgroundColor(colors.black)
+	
+end 
