5778fc3243c83df26b8a66ff4b821ee66ebf621c
ValDin08 <valentin.dinon@gmail.com>
2025-08-05 14:21:09 +0200

    Create pixelLink 1.0.lua

---
+++ pixelLink.lua
@@ -0,0 +1,102 @@
+local pixelLink = {}
+
+local lastMessageID = 0
+
+-- FONCTIONS
+    --Traçabilité messages
+    local function nextMessageID()
+        lastMessageID = lastMessageID + 1
+        return lastMessageID
+    end
+
+    -- Fonctions d'envoi de message
+        -- Envoi simple (fire and forget)
+            function pixelLink.send(msgType, srcType, dstID, payload)
+                local message = {
+                    msgID = nextMessageID(),
+                    msgType = msgType,
+                    srcType = srcType,
+                    srcID   = os.getComputerID(),
+                    dstID   = dstID,
+                    ts      = os.epoch("utc"),
+                    payload = payload
+                }
+                    if dstID then                        
+                        rednet.send(dstID, message) -- Envoi en Simplecast
+
+                    else
+                        rednet.broadcast(message) -- Envoi en Broadcast
+                    end
+
+            end
+
+        -- Envoi avec attente de réponse
+            function pixelLink.request(msgType, srcType, dstID, payload, timeout)
+                local message = {
+                    msgID = nextMessageID(),
+                    msgType = msgType,
+                    srcType = srcType,
+                    srcID   = os.getComputerID(),
+                    dstID   = dstID,
+                    ts      = os.epoch("utc"),
+                    payload = payload
+                }
+                -- Envoie la requête
+                rednet.send(dstID, message)
+
+                -- Attend une réponse pendant un temps (par défaut 5 secondes)
+                local t = timeout or 5
+                local id, receivedMessage = rednet.receive(t)
+
+                -- Vérifie qu'une réponse a bien été reçue, et de la bonne source
+                if id == dstID and type(receivedMessage) == "table" then -- Vérification si le message reçu est bien sous la forme d'une table
+                    -- Selon le type de la réponse
+                    if receivedMessage.msgType == "auth" then -- Demande d'autorisation
+                        return true, receivedMessage.payload
+
+                    elseif receivedMessage.msgType == "command" then -- Demande de commande
+                        return true, receivedMessage.payload
+
+                    elseif receivedMessage.msgType == "connect" then -- Demande de connexion
+                        return true
+
+                    else -- Type inconnu
+                        return false, receivedMessage
+
+                    end
+
+                else
+                    -- Pas de réponse ou pas du bon ID
+                    return false, nil
+
+                end
+
+            end
+
+
+    -- Fonctions de réception des messages
+        function pixelLink.receive(srcType)
+            local id, receivedMessage = rednet.receive() -- Attend un message
+            if type(receivedMessage) == "table" then -- Vérification si le message reçu est bien sous la forme d'une table
+                if receivedMessage.msgType == "connect" then -- Demande de connexion
+                    payload = {}
+                    pixelLink.send("connect", srcType, id, payload) -- Envoi de la réponse de connexion
+
+                elseif receivedMessage.msgType == "auth" then -- Demande d'autorisation de travail
+                    local authorization = server.authorization()
+                    payload = {authorization}
+                    pixelLink.send("auth", srcType, id, payload) -- Envoi de la réponse d'autorisation de travail
+
+                elseif receivedMessage.msgType == "status" then -- Reception de statut
+                    server.updateHMI(receivedMessage) -- Mise à jour de l'IHM et des données serveur
+
+                else
+                    return false
+
+                end
+
+            end
+
+        end
+
+return pixelLink
